705d2161b999c336d7ef9d82e70ca2ee94f5628b
fib4 <90240770+fib4@users.noreply.github.com>
2022-01-28 18:55:08 +0200

    add dynamic dropdown menu

--- main.js
+++ main.js
@@ -1,10 +1,13 @@
-const addButton = document.querySelector('#add_button');
+const addForm = document.querySelector('#add_name_form');
 const addNameBox = document.querySelector('#add_name_box');
+const dropDownMenu = document.querySelector('#number_per_team');
 const namelist = [];
 
+console.log(addForm.new_input)
+
  function addNewName() {
-   const input = document.querySelector('#new_input');
-   const name = input.value;
+   const input = addForm.new_input
+   const name = input.value
    if (namelist.length === 0) {
      const newNameList = document.createElement('ul');
      newNameList.className = 'namelist';
@@ -17,8 +20,22 @@ const namelist = [];
    input.value = "";
  }
 
-console.log(addButton);
+ function calculateTeams() {
+
+   const maxplayers = Math.ceil(namelist.length / 2);
+   console.log(dropDownMenu.children.length)
+   if (maxplayers <= 1 || dropDownMenu.children.length === maxplayers) {
+     return;
+   }
+   dropDownMenu.insertAdjacentHTML('beforeend', `<option value="${maxplayers}">${maxplayers}</option>`)
+ }
+
+
 console.log(addNameBox);
 console.log(namelist)
 
-addButton.addEventListener('click', addNewName);
+addForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  addNewName();
+  calculateTeams();
+});
