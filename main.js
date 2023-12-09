c92b1e68e39ad3500206dcca14bb1d8f468124b0
fib4 <90240770+fib4@users.noreply.github.com>
2023-12-09 11:57:17 +0200

    don't do anything on empty inputs

--- main.js
+++ main.js
@@ -1,67 +1,63 @@
-const addForm = document.querySelector('#add_name_form');
-const addNameBox = document.querySelector('#add_name_box');
-const dropDownMenu = document.querySelector('#number_per_team');
+const addForm = document.querySelector("#add_name_form");
+const addNameBox = document.querySelector("#add_name_box");
+const dropDownMenu = document.querySelector("#number_per_team");
 const namelist = [];
-const generateButton = document.querySelector('#generateButton')
+const generateButton = document.querySelector("#generateButton");
 
+function addNewName() {
+  const input = addForm.new_input;
+  const name = input.value;
+  if (!input.value) {
+    return;
+  }
+  if (namelist.length === 0) {
+    const newNameList = document.createElement("ul");
+    newNameList.className = "namelist";
+    newNameList.setAttribute("id", "initialnamelist");
+    addNameBox.appendChild(newNameList);
+  }
+  namelist.push(name);
+  const namelistelement = document.querySelector("#initialnamelist");
+  namelistelement.insertAdjacentHTML("beforeend", `<li>${name}</li>`);
+  input.value = "";
+}
 
-console.log(addForm.new_input)
+function calculateTeams() {
+  const maxplayers = Math.ceil(namelist.length / 2);
+  if (maxplayers <= 1 || dropDownMenu.children.length === maxplayers) {
+    return;
+  }
+  dropDownMenu.insertAdjacentHTML("beforeend", `<option value="${maxplayers}">${maxplayers}</option>`);
+}
 
- function addNewName() {
-   const input = addForm.new_input
-   const name = input.value
-   if (namelist.length === 0) {
-     const newNameList = document.createElement('ul');
-     newNameList.className = 'namelist';
-     newNameList.setAttribute('id', 'initialnamelist');
-     addNameBox.appendChild(newNameList);
-   }
-   namelist.push(name);
-   const namelistelement = document.querySelector('#initialnamelist');
-   namelistelement.insertAdjacentHTML('beforeend', `<li>${name} <button class="removebutton">x</button></li>`);
-   input.value = "";
- }
-
- function calculateTeams() {
-
-   const maxplayers = Math.ceil(namelist.length / 2);
-   console.log(dropDownMenu.children.length)
-   if (maxplayers <= 1 || dropDownMenu.children.length === maxplayers) {
-     return;
-   }
-   dropDownMenu.insertAdjacentHTML('beforeend', `<option value="${maxplayers}">${maxplayers}</option>`)
- }
-
- function generateTeams() {
-   const a = [...namelist]
+function generateTeams() {
+  if (namelist.length === 0 || !dropDownMenu.value) {
+    return;
+  }
+  const a = [...namelist];
   for (let i = a.length - 1; i > 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1));
-      [a[i], a[j]] = [a[j], a[i]];
+    const j = Math.floor(Math.random() * (i + 1));
+    [a[i], a[j]] = [a[j], a[i]];
   }
 
-  console.log(a)
-  console.log(namelist)
-  const teams = document.querySelector('#teams')
+  const teams = document.querySelector("#teams");
+  teams.innerHTML = "";
 
-  while (a.length != 0){
-    const newteam = document.createElement('ul')
-    newteam.className = 'namelist teamlist'
-    const players = a.splice(0, dropDownMenu.value)
-    players.map(player => {
-      newteam.insertAdjacentHTML('beforeend', `<li class="teamlistelement">${player}</li>`)
-    })
-    teams.appendChild(newteam)
+  while (a.length != 0) {
+    const newteam = document.createElement("ul");
+    newteam.className = "namelist teamlist";
+    const players = a.splice(0, dropDownMenu.value);
+    players.map((player) => {
+      newteam.insertAdjacentHTML("beforeend", `<li class="teamlistelement">${player}</li>`);
+    });
+    teams.appendChild(newteam);
   }
 }
 
-
-console.log(addNameBox);
-console.log(namelist)
-
-addForm.addEventListener('submit', (e) => {
+addForm.addEventListener("submit", (e) => {
   e.preventDefault();
   addNewName();
   calculateTeams();
 });
 
-generateButton.addEventListener('click', generateTeams);
+generateButton.addEventListener("click", generateTeams);
